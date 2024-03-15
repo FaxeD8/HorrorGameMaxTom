26f5663c9c320674bb18a8cec6a911b9d9d680a2
FaxeD8 <137116896+FaxeD8@users.noreply.github.com>
2024-03-15 12:35:02 +0100

    Lampe fonctione avec F
    F juste

---
+++ Assets/Script/Flashlight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public GameObject Light;
+    public bool LightOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LightOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f") && LightOn == false)
+        {
+            Debug.Log("Fonctionne");
+            LightOn = true;
+            Light.SetActive(true);
+        }
+        else if (Input.GetKeyDown("f") && LightOn == true)
+        {
+            Debug.Log("Eteint");
+            LightOn = false;
+            Light.SetActive(false);
+        }
+
+    }
+}
